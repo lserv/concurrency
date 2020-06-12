3d2e5511f4a673b624cd32fda8c4b6d484917a75
lenur <lenur.serveriev@gmail.com>
2020-06-12 09:17:09 +0300

    implemented semaphore example

---
+++ multithreading/src/main/java/io/lenur/concurrency/multithreading/semaphore/Application.java
@@ -0,0 +1,14 @@
+package io.lenur.concurrency.multithreading.semaphore;
+
+import java.util.concurrent.Semaphore;
+
+public class Application {
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(2);
+        new Philosopher(semaphore,"Socrates").start();
+        new Philosopher(semaphore,"Plato").start();
+        new Philosopher(semaphore,"Aristotle").start();
+        new Philosopher(semaphore,"Thales").start();
+        new Philosopher(semaphore,"Pythagoras").start();
+    }
+}
---
+++ multithreading/src/main/java/io/lenur/concurrency/multithreading/semaphore/Philosopher.java
@@ -0,0 +1,34 @@
+package io.lenur.concurrency.multithreading.semaphore;
+
+import java.util.concurrent.Semaphore;
+
+class Philosopher extends Thread {
+    private final Semaphore semaphore;
+    private final String name;
+    private boolean ate = false;
+
+   Philosopher(Semaphore semaphore, String name) {
+       this.semaphore = semaphore;
+       this.name=name;
+   }
+
+   @Override
+   public void run() {
+       try {
+           if (!ate) {
+               semaphore.acquire();
+               System.out.println (name + " sit down to the table");
+
+               sleep(300);
+               ate = true;
+
+               System.out.println (name + " ate! He is getting up from the table");
+               semaphore.release();
+
+               sleep(300);
+           }
+       } catch(InterruptedException e) {
+           e.printStackTrace();
+       }
+   }
+}
