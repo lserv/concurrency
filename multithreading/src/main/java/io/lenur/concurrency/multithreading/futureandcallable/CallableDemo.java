c4dc58219bd5f4366e0ce16aa1c73f78b229d959
lenur <lenur.serveriev@gmail.com>
2020-06-15 09:04:40 +0300

    implemented callable demo

---
+++ multithreading/src/main/java/io/lenur/concurrency/multithreading/futureandcallable/CallableDemo.java
@@ -0,0 +1,16 @@
+package io.lenur.concurrency.multithreading.futureandcallable;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+public class CallableDemo {
+    public static void main(String[] args) throws Exception {
+        Callable<Integer> callable = () -> {
+            TimeUnit.SECONDS.sleep(1);
+            return 5;
+        };
+        int result = callable.call();
+
+        System.out.println(result);
+    }
+}
