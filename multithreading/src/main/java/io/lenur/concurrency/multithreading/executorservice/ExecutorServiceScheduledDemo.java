ab64b0bc9ca647cbe1e606a0369a6538581c0ec8
lenur <lenur.serveriev@gmail.com>
2020-06-15 08:41:16 +0300

    implemented scheduled executor demo

---
+++ multithreading/src/main/java/io/lenur/concurrency/multithreading/executorservice/ExecutorServiceScheduledDemo.java
@@ -0,0 +1,22 @@
+package io.lenur.concurrency.multithreading.executorservice;
+
+import java.time.LocalTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ExecutorServiceScheduledDemo {
+    public static void main(String[] args) {
+        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+        Runnable command = () -> System.out.println(LocalTime.now() + ": Hello!");
+        int initDelay = 1000;
+        int period = 1000;
+        TimeUnit unit = TimeUnit.MILLISECONDS;
+        executor.scheduleAtFixedRate(command, initDelay, period, unit);
+        //08:39:49.221961: Hello!
+        //08:39:50.123106: Hello!
+        //08:39:51.122829: Hello!
+        //08:39:52.122822: Hello!
+        //...
+    }
+}
--- multithreading/src/main/java/io/lenur/concurrency/multithreading/executorservice/ExecutorServiceSingleThreadDemo.java
+++ multithreading/src/main/java/io/lenur/concurrency/multithreading/executorservice/ExecutorServiceSingleThreadDemo.java
@@ -36,7 +36,7 @@ public class ExecutorServiceSingleThreadDemo {
 
     private static class NumberUtil {
         public static boolean isPrime(int number) {
-            for (int i = 2; i<=number/2; i++) {
+            for (int i = 2; i <= number/2; i++) {
                 if (number % i == 0) {
                     return false;
                 }
