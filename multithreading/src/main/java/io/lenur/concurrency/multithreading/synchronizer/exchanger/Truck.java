369d77b3a506e0cf6c1b0927b9f6a1626afafc08
lenur <lenur.serveriev@gmail.com>
2020-07-25 07:09:54 +0300

    reduced seconds into milliseconds in synchronizer exchanger

--- multithreading/src/main/java/io/lenur/concurrency/multithreading/synchronizer/exchanger/Truck.java
+++ multithreading/src/main/java/io/lenur/concurrency/multithreading/synchronizer/exchanger/Truck.java
@@ -29,12 +29,12 @@ public class Truck implements Runnable {
         try {
             System.out.printf("To the truck №%d loaded: %s и %s.\n", number, parcels[0], parcels[1]);
             System.out.printf("The truck №%d moved from point %s to point %s.\n", number, dep, destination);
-            TimeUnit.SECONDS.sleep(1000 + (long) (Math.random() * 5000));
+            TimeUnit.MILLISECONDS.sleep(1000 + (long) (Math.random() * 5000));
             System.out.printf("The truck №%d has come to the point Е.\n", number);
             parcels[1] = exchanger.exchange(parcels[1]);//when we perform exchange() a thread blocks and waits
             //while another thread calls exchange(), after that there will be an exchange of packages
             System.out.printf("Into the truck №%d placed parcel for point %s.\n", number, destination);
-            TimeUnit.SECONDS.sleep(1000);
+            TimeUnit.MILLISECONDS.sleep(1000);
             System.out.printf("The truck №%d has come into %s and delivered: %s и %s.\n", number, destination, parcels[0], parcels[1]);
         } catch (InterruptedException e) {
             e.printStackTrace();
